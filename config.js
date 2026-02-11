24789e680917fd0caa439e55a92632fbe0495fc2
CFAT <shifatsrm09@gmail.com>
2026-02-11 19:18:57 +0600

    server live on render

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // LOCAL DEVELOPMENT
-const YSYNC_SERVER = "ws://localhost:3000";
+//const YSYNC_SERVER = "ws://localhost:3000";
 
 // PRODUCTION (Uncomment when deploying)
-// const YSYNC_SERVER = "wss://your-render-url.onrender.com";
+const YSYNC_SERVER = "wss://ysync-server.onrender.com";
