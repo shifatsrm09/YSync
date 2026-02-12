68058ea5bccfb9d19c9b98ac5a8a4099476c6656
CFAT <shifatsrm09@gmail.com>
2026-02-12 20:06:10 +0600

    Sleep time, Refresh, Ui, config fixed | 777

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
-// LOCAL DEVELOPMENTconst YSYNC_SERVER = "ws://localhost:3000";
+// LOCAL DEVELOPMENTconst
+// YSYNC_SERVER = "ws://localhost:3000";
 
 // PRODUCTION (Uncomment when deploying)
 const YSYNC_SERVER = "wss://ysync-server.onrender.com";
